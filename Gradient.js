583e4ccab1b3bc5584b091c0e5a83c3a64ea6c0e
Disruptive-Social <53658338+Disruptive-Social@users.noreply.github.com>
2022-05-07 01:07:40 +0100

    Update Gradient.js

--- Gradient.js
+++ Gradient.js
@@ -1,6 +1,5 @@
 
 /*
-*   Stripe WebGl Gradient Animation by Stripe.com
 *   ScrollObserver functionality to disable animation when not scrolled into view has been disabled and 
 *   commented out for now.
 */
